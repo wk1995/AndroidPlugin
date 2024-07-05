cebae9281e65281abb210ee612908a0b59ee8b06
wangkang <1226426603@qq.com>
2024-07-05 18:03:19 +0800

    支持插件发布

--- slowMethod/build.gradle.kts
+++ slowMethod/build.gradle.kts
@@ -6,33 +6,14 @@ plugins {
 
 PublishInfo {
     groupId = "custom.android.plugin"
+    artifactId = "monitor-slowMethod"
+    version = "0.0.2"
+    pluginId = "plugin.android.monitor.slow.method"
+    implementationClass = "custom.android.plugin.SlowMethodMonitorPlugin"
 }
 dependencies {
     //gradle sdk
     implementation(gradleApi())
     //groovy sdk
     implementation(localGroovy())
-}
-
-gradlePlugin {
-    plugins {
-        create("slowMethodMonitorPlugin") {
-            // 插件ID
-            id = "plugin.android.monitor.slow.method"
-            // 插件的实现类
-            implementationClass = "custom.android.plugin.SlowMethodMonitorPlugin"
-        }
-    }
-
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("SlowMethodMonitorPlugin") {
-            groupId = "custom.android.plugin"
-            artifactId = "monitor-slowMethod"
-            version = "0.0.1"
-            from(components["java"])
-        }
-    }
 }
