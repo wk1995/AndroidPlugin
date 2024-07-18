fb6867505e3a3716f8246818f64d955330a331dc
wk <1226426603@qq.com>
2024-07-18 20:54:12 +0800

    remove invalid code

--- app/src/main/java/com/wk/plugin/MainActivity.kt
+++ app/src/main/java/com/wk/plugin/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.wk.plugin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import cn.entertech.affective.sdk.api.IAffectiveDataAnalysisService
-import cn.entertech.affective.sdk.bean.AffectiveServiceWay
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    private val affectiveService by lazy {
-        IAffectiveDataAnalysisService.getService(AffectiveServiceWay.AffectiveLocalService)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "affectiveService: ${affectiveService?.hasConnectAffectiveService()}")
     }
 }
