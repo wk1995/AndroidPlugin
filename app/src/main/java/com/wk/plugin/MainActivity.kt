e2e6cc7902168e70b6a313bd267e302be729de85
wk <1226426603@qq.com>
2024-08-14 20:34:46 +0800

    移除无用代码

--- app/src/main/java/com/wk/plugin/MainActivity.kt
+++ app/src/main/java/com/wk/plugin/MainActivity.kt
@@ -2,18 +2,12 @@ package com.wk.plugin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import cn.entertech.affective.sdk.api.IAffectiveDataAnalysisService
-import cn.entertech.affective.sdk.bean.AffectiveServiceWay
 
 class MainActivity : AppCompatActivity() {
-    private val affectiveService by lazy {
-        IAffectiveDataAnalysisService.getService(AffectiveServiceWay.AffectiveLocalService)
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "affectiveService: ${affectiveService?.hasConnectAffectiveService()}")
     }
 }
